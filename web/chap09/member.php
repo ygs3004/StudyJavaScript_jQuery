f3c5e7d2c92180df638bb018ed4a005cec553d00
GunSoo Yoon <ygs3004@naver.com>
2022-09-12 05:17:22 +0900

    220912

---
+++ web/chap09/member.php
@@ -0,0 +1,9 @@
+<?php
+    $data='[
+        {"id":"1", "name":"Choldcroft", "email":"dcroft0@hibu.com"},
+        {"id":"2", "name":"Levi", "email":"wlevi1@blogger.com"},
+        {"id":"3", "name":"Axcell", "email":"laxcellc@ebay.co.kr"}
+    ]';
+
+    echo $_GET["callback"]."(".$data.")";
+?>
---
+++ web/chap09/news.php
@@ -0,0 +1,11 @@
+<?php
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_URL, "http://myhome.chosun.com/rss/www_section_rss.xml");
+    $url_source=curl_exec($ch);
+    curl_close($ch);
+
+    echo $url_source;
+?>
