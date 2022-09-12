c9d50420a6e3c92d3f58876c0d0074b579c62d30
GunSoo Yoon <ygs3004@naver.com>
2022-09-12 18:43:08 +0900

    220912

---
+++ out/artifacts/JavaScript_jQuery_war_exploded/chap11/js/main.js
@@ -0,0 +1,24 @@
+$(function(){
+    $(".main-visual-slide").bxSlider({
+        auto:true,
+        pause:2000,
+        autoHover:true,
+        autoControls:true,
+        autoControlsCombine:true
+    })
+
+    $("#notice-tab-wrap h4 a").on("click", tapmenu);
+
+    function tabmenu(e){
+        e.preventDefault();
+        var $ts = $(this);
+        var $next = $ts.parent().next();
+
+        if($next.is(":hidden")){
+            $("#notice-tab-wrap h4 a").removeClass("on");
+            $ts.addClass("on");
+            $("#notice-tab-wrap > div:visible").hide();
+            $next.show();
+        }
+    }
+})
